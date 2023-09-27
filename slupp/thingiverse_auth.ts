d6b685925aca3e2e9d29a8363f8225ad126ef7cb
Ben Mordue <benmordue@gmail.com>
2023-09-27 13:15:55 +0100

    first dump

---
+++ slupp/thingiverse_auth.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// Thingiverse API credentials
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = 'http://localhost:3000/auth/callback';
+
+// Step 1: Redirect to Thingiverse for Authorization
+app.get('/auth', (req, res) => {
+  const authorizeUrl = `https://www.thingiverse.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  res.redirect(authorizeUrl);
+});
+
+// Step 2: Handle OAuth2 Callback
+app.get('/auth/callback', async (req, res) => {
+  const code = req.query.code as string;
+
+  // Step 3: Exchange code for an access token
+  const tokenResponse = await axios.post('https://www.thingiverse.com/login/oauth/access_token', null, {
+    params: {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: REDIRECT_URI,
+    },
+  });
+
+  const accessToken = tokenResponse.data.access_token;
+
+  // Step 4: Use the access token to make authenticated requests to the API
+  try {
+    const response = await axios.get('https://api.thingiverse.com/users/following', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const followedUsers = response.data;
+    res.json(followedUsers);
+  } catch (error) {
+    res.status(500).send('Error fetching followed users.');
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
