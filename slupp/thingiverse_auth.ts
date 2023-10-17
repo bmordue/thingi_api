de25aa474d32a4179879085a0cc43d19e1ff79b4
Ben Mordue <benmordue@gmail.com>
2023-10-17 22:01:42 +0100

    slupp

--- slupp/thingiverse_auth.ts
+++ slupp/thingiverse_auth.ts
@@ -2,66 +2,76 @@ import axios from 'axios';
 import express from 'express';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+function main() {
+  dotenv.config();
 
-const app = express();
-const port = 3000;
+  const app = express();
+  const port = 3000;
 
-// Thingiverse API credentials
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = 'http://localhost:3000/auth/callback';
+  // Thingiverse API credentials
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const CLIENT_SECRET = process.env.CLIENT_SECRET;
+  const REDIRECT_URI = 'http://localhost:3000/auth/callback';
 
-// Step 1: Redirect to Thingiverse for Authorization
-app.get('/auth', (req, res) => {
-  const authorizeUrl = `https://www.thingiverse.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-  res.redirect(authorizeUrl);
-});
-
-// Step 2: Handle OAuth2 Callback
-app.get('/auth/callback', async (req, res) => {
-  const code = req.query.code as string;
-
-  // Step 3: Exchange code for an access token
-  const tokenResponse = await axios.post('https://www.thingiverse.com/login/oauth/access_token', null, {
-    params: {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      code,
-      grant_type: 'authorization_code',
-      redirect_uri: REDIRECT_URI,
-    },
+  // Step 1: Redirect to Thingiverse for Authorization
+  app.get('/auth', (req, res) => {
+    const authorizeUrl = `https://www.thingiverse.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    res.redirect(authorizeUrl);
   });
 
-  const accessToken = tokenResponse.data.split('=')[1].split('&')[0];
+  // Step 2: Handle OAuth2 Callback
+  app.get('/auth/callback', async (req, res) => {
+    const code = req.query.code as string;
 
-  // step 3b: validate token
-  const validateResponse = await axios.post('https://www.thingiverse.com/login/oauth/tokeninfo', null, {
-    params: { access_token: accessToken }
-  });
-
-  // Step 4: Use the access token to make authenticated requests to the API
-  // https://api.thingiverse.com/users/following
-  try {
-    const response = await axios.get('https://api.thingiverse.com/users/me/following', {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+    // Step 3: Exchange code for an access token
+    const tokenResponse = await axios.post('https://www.thingiverse.com/login/oauth/access_token', null, {
+      params: {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code,
+        grant_type: 'authorization_code',
+        redirect_uri: REDIRECT_URI,
       },
     });
 
-    const followedUsers = response.data;
+    const accessToken = tokenResponse.data.split('=')[1].split('&')[0];
 
-    followedUsers.forEach(following => {
-      //following
+    // step 3b: validate token
+    const validateResponse = await axios.post('https://www.thingiverse.com/login/oauth/tokeninfo', null, {
+      params: { access_token: accessToken }
     });
 
-    res.json(followedUsers);
-  } catch (error) {
-    res.status(500).send('Error fetching followed users.');
-  }
-});
+    // Step 4: Use the access token to make authenticated requests to the API
+    // https://api.thingiverse.com/users/following
+    try {
+      const response = await axios.get('https://api.thingiverse.com/users/me/following', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+      const followedUsers = response.data;
+
+      followedUsers.forEach(following => {
+        //queue downloads for each of the followed user's objects
+      });
+
+      res.json(followedUsers);
+    } catch (error) {
+      res.status(500).send('Error fetching followed users.');
+    }
+
+    // Step 5: find all starred objects that do not belong to a followed user.
+    // get starred objects
+    // for each starred object, check whether the owner is in followedUsers
+    // if not, queue download for that object
+  });
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+}
+
+main();
